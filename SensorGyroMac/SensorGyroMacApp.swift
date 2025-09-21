573f72d7d963c1a78d78e76cade1a0fa9c05e3fa
Ramiro Sanabria <ramineu@icloud.com>
2025-09-20 23:37:18 -0300

    Ya compila y se ejecuta, pero no detecta nada.

---
+++ SensorGyroMac/SensorGyroMacApp.swift
@@ -0,0 +1,16 @@
+//
+//  SensorGyroApp.swift
+//  SensorGyroMac
+//
+//  Created by Ramiro Nehuen Sanabria on 20/09/2025.
+//
+
+import SwiftUI
+
+struct SensorGyroMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SensorGyroMac/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SensorGyroMac
+//
+//  Created by Ramiro Nehuen Sanabria on 20/09/2025.
+//
+
+import Foundation
+import SwiftUI
+
+SensorGyroMacApp.main()
